2981c8b8cbdf9869ddbc473059d29f1ad550a8c9
mateuszswierczewski <48724224+mateuszswierczewski@users.noreply.github.com>
2021-10-31 16:55:39 +0100

    Initial commit. Project setup.

---
+++ src/main/java/pl/mswierczewski/inclouds/InCloudsApplication.java
@@ -0,0 +1,13 @@
+package pl.mswierczewski.inclouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InCloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InCloudsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/mswierczewski/inclouds/InCloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mswierczewski.inclouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InCloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
